a02ddfd0bd0b426213ed273e70e8ca36a22903e4
ngochiep30051998 <35429836+ngochiep30051998@users.noreply.github.com>
2019-03-26 23:27:16 +0700

    fake dữ liệu cho chức năng đăng nhập, đăng kí

--- WebBanSach/Controllers/DangKiController.cs
+++ WebBanSach/Controllers/DangKiController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebBanSach.Models.EF;
 
 namespace WebBanSach.Controllers
 {
@@ -12,6 +13,17 @@ namespace WebBanSach.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+        public ActionResult postSignUp(string HoTen, string Email, string MatKhau)
+        {
+            List<Khachhang> DanhSachKhachHang = new List<Khachhang>();
+            Khachhang kh = new Khachhang();
+            kh.Hotenkh = HoTen;
+            kh.Email = Email;
+            kh.Matkhau = MatKhau;
+            DanhSachKhachHang.Add(kh);
+            Session["user"] = kh;
+            return RedirectToAction("Index", "DangNhap");
         }
     }
 }
--- WebBanSach/Controllers/DangNhapController.cs
+++ WebBanSach/Controllers/DangNhapController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebBanSach.Models.EF;
 
 namespace WebBanSach.Controllers
 {
@@ -11,7 +12,26 @@ namespace WebBanSach.Controllers
         // GET: DangNhap
         public ActionResult Index()
         {
-            return View();
+            if (Session["user"] != null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                return View();
+            }
+            
+        }
+        public ActionResult postLogin(string email, string password)
+        {
+            Khachhang kh = new Khachhang();
+            kh.Email = "abc@gmail.com";
+            kh.Matkhau = "123";
+            if(email.Equals(kh.Email) && password.Equals(kh.Matkhau))
+            {
+                Session["user"] = kh;
+            }
+            return RedirectToAction("Index");
         }
     }
 }
