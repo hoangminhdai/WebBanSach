aff91c49e0e4c9b2f30b956aff81f9f0865fb848
truongnguyenuet <truong.nguyenvan@powergatesoftware.com>
2019-04-04 21:00:18 +0700

    Update page Category

--- WebBanSach/Controllers/DanhMucController.cs
+++ WebBanSach/Controllers/DanhMucController.cs
@@ -8,168 +8,22 @@ namespace WebBanSach.Controllers
 {
     public class DanhMucController : Controller
     {
+        QuanLyBanSachDbContext db = new QuanLyBanSachDbContext();
         // GET: DanhMuc
         public ActionResult Index()
         {
-            List<Chude> lstDanhMuc = new List<Chude>();
-            lstDanhMuc.Add(new Chude()
-            {
-                Tenchude = "Sách giáo khoa",
-                Key = "app"
-            });
-            lstDanhMuc.Add(new Chude()
-            {
-                Tenchude = "Sách thiếu nhi",
-                Key = "web"
-            });
-            lstDanhMuc.Add(new Chude()
-            {
-                Tenchude = "Sách khoa học",
-                Key = "logo"
-            });
-            lstDanhMuc.Add(new Chude()
-            {
-                Tenchude = "Sách khác",
-                Key = "card"
-            });
-
-            List<Sach> lstSach = new List<Sach>();
-            lstSach.Add(new Sach()
-            {
-                Hinhminhhoa = "k.jpg",
-                Tensach = "Thuật tư tưởng",
-                Giakm = 690000,
-                Dongia = 960000,
-                Chude = new Chude() { Key = "web" }
-            });
-            lstSach.Add(new Sach()
-            {
-                Hinhminhhoa = "gk.jpg",
-                Tensach = "Đứa bé vẫn sống",
-                Giakm = 690000,
-                Dongia = 960000,
-                Chude = new Chude() { Key = "app" }
-            });
-            lstSach.Add(new Sach()
-            {
-                Hinhminhhoa = "z.jpg",
-                Tensach = "Sau màn đêm",
-                Giakm = 690000,
-                Dongia = 960000,
-                Chude = new Chude() { Key = "logo" }
-            });
-            lstSach.Add(new Sach()
-            {
-                Hinhminhhoa = "t.jpg",
-                Tensach = "Một bước yêu, vạn dặm đau",
-                Giakm = 690000,
-                Dongia = 960000,
-                Chude = new Chude() { Key = "app" }
-            });
-            lstSach.Add(new Sach()
-            {
-                Hinhminhhoa = "tt.jpg",
-                Tensach = "Một bước yêu, vạn dặm đau",
-                Giakm = 690000,
-                Dongia = 960000,
-                Chude = new Chude() { Key = "card" }
-            });
-            lstSach.Add(new Sach()
-            {
-                Hinhminhhoa = "zz.jpg",
-                Tensach = "Hồng nhan",
-                Giakm = 690000,
-                Dongia = 960000,
-                Chude = new Chude() { Key = "logo" }
-            });
-            lstSach.Add(new Sach()
-            {
-                Hinhminhhoa = "gk6.jpg",
-                Tensach = "Người từng quen",
-                Giakm = 690000,
-                Dongia = 960000,
-                Chude = new Chude() { Key = "web" }
-            });
-            lstSach.Add(new Sach()
-            {
-                Hinhminhhoa = "ttt.jpg",
-                Tensach = "Tôi đi code dạo",
-                Giakm = 690000,
-                Dongia = 960000,
-                Chude = new Chude() { Key = "card" }
-            });
-            lstSach.Add(new Sach()
-            {
-                Hinhminhhoa = "gk3.jpg",
-                Tensach = "From zero to hero",
-                Giakm = 690000,
-                Dongia = 960000,
-                Chude = new Chude() { Key = "app" }
-            });
-            lstSach.Add(new Sach()
-            {
-                Hinhminhhoa = "tttt.jpg",
-                Tensach = "Tâm lí học phụ nữ",
-                Giakm = 690000,
-                Dongia = 960000,
-                Chude = new Chude() { Key = "logo" }
-            });
-            lstSach.Add(new Sach()
-            {
-                Hinhminhhoa = "gk4.jpg",
-                Tensach = "Dế Mèn phiêu lưu kí",
-                Giakm = 690000,
-                Dongia = 960000,
-                Chude = new Chude() { Key = "app" }
-            });
-            lstSach.Add(new Sach()
-            {
-                Hinhminhhoa = "ttttt.jpg",
-                Tensach = "Tiếng gà trưa",
-                Giakm = 690000,
-                Dongia = 960000,
-                Chude = new Chude() { Key = "web" }
-            });
-            lstSach.Add(new Sach()
-            {
-                Hinhminhhoa = "gk5.jpg",
-                Tensach = "CCNA Sercurity",
-                Giakm = 690000,
-                Dongia = 960000,
-                Chude = new Chude() { Key = "logo" }
-            });
-            lstSach.Add(new Sach()
-            {
-                Hinhminhhoa = "kk.jpg",
-                Tensach = "Kinh thánh",
-                Giakm = 690000,
-                Dongia = 960000,
-                Chude = new Chude() { Key = "card" }
-            });
-            lstSach.Add(new Sach()
-            {
-                Hinhminhhoa = "kkk.jpg",
-                Tensach = "Những bài ca bất hủ",
-                Giakm = 690000,
-                Dongia = 960000,
-                Chude = new Chude() { Key = "web" }
-            });
-            lstSach.Add(new Sach()
-            {
-                Hinhminhhoa = "kkkk.jpg",
-                Tensach = "Tôi và em",
-                Giakm = 690000,
-                Dongia = 960000,
-                Chude = new Chude() { Key = "logo" }
-            });
-
-
-
+            var lstDanhMuc = db.Chudes.ToList();
+            var lstSach = db.Saches.ToList();
 
             ViewBag.ListDanhMuc = lstDanhMuc;
-            ViewBag.ListSach = lstSach;
+            return View(lstSach);
+        }
 
-            return View();
+        [ChildActionOnly]
+        public ActionResult SachLienQuan()
+        {
+            var listSachLienQuan = db.Saches.Take(5).ToList();
+            return PartialView("_SanPhamLienQuan", listSachLienQuan);
         }
     }
 }
