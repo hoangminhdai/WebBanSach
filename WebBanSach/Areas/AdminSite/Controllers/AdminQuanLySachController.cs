ba5ce7bf80c035e69e61e97dbd160b5cc48c4839
ngochiep30051998 <35429836+ngochiep30051998@users.noreply.github.com>
2019-04-07 10:53:28 +0700

    hiển thị danh sách sản phẩm

--- WebBanSach/Areas/AdminSite/Controllers/AdminQuanLySachController.cs
+++ WebBanSach/Areas/AdminSite/Controllers/AdminQuanLySachController.cs
@@ -3,19 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebBanSach.Models.EF;
 
 namespace WebBanSach.Areas.AdminSite.Controllers
 {
     public class AdminQuanLySachController : Controller
     {
+        QuanLyBanSachDbContext db = new QuanLyBanSachDbContext();
+
         // GET: AdminSite/AdminQuanLySach
         public ActionResult TatCaSach()
         {
+            var TatCaSach = db.Saches.Join(db.Nhaxuatbans, sach => sach.Manxb, nxb => nxb.Manxb, (sach, nxb) => sach)
+                                      .Join(db.Chudes, sach => sach.Macd, chude => chude.Macd, (sach, chude) => sach)
+                                      .Join(db.Tacgias, sach => sach.Matacgia, tacgia => tacgia.Matacgia, (sach, tacgia) => sach)
+                                      .ToList();
+            ViewBag.TatCaSach = TatCaSach;
             return View();
         }
+        public ActionResult ViewThemSach()
+        {
+            var TatCaChuDe = db.Chudes.ToList();
+            var TatCaTacGia = db.Tacgias.ToList();
+            var TatCaNXB = db.Nhaxuatbans.ToList();
+            ViewBag.TatCaChuDe = TatCaChuDe;
+            ViewBag.TatCaTacGia = TatCaTacGia;
+            ViewBag.TatCaNXB = TatCaNXB;
+            return View();
+        }
+        [HttpPost]
         public ActionResult ThemSach()
         {
-            return View();
+            Sach sach = new Sach();
+            return RedirectToAction("TatCaSach");
+            
         }
         public ActionResult SuaSach()
         {
--- WebBanSach/Areas/AdminSite/Controllers/AdminTrangChuController.cs
+++ WebBanSach/Areas/AdminSite/Controllers/AdminTrangChuController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebBanSach.Models.EF;
 
 namespace WebBanSach.Areas.AdminSite.Controllers
 {
