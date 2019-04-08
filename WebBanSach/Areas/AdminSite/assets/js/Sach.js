efb65f7c498167e2bcff82c132feed24c82d7be0
ngochiep30051998 <35429836+ngochiep30051998@users.noreply.github.com>
2019-04-08 23:59:33 +0700

    thêm, sửa, xóa sách, chưa validate form

---
+++ WebBanSach/Areas/AdminSite/assets/js/Sach.js
@@ -0,0 +1,23 @@
+﻿// xóa sách
+$('#myModal').on('show.bs.modal', (e) => {
+    const link = e.relatedTarget;
+    let MaSach = parseInt(jQuery(link).attr('id'));
+    jQuery('#btnDelete').click(() => {
+        jQuery.ajax({
+            url: '/AdminQuanLySach/XoaSach',
+            method: 'POST',
+            data: { MaSach },
+            dataType: 'text',
+            success: (response) => {
+                response = JSON.parse(response)
+                if (response.status == true) {
+                    jQuery('#myModal').modal('hide');
+                    alert("xóa thành công");
+                    location.reload();
+                } else {
+                    alert('Xóa sản phẩm thất bại!')
+                }
+            }
+        })
+    })
+});
---
+++ WebBanSach/Areas/AdminSite/assets/js/ThemSach.js
@@ -0,0 +1,17 @@
+﻿
+const onFileSelected = (event) => {
+    var selectedFile = event.target.files[0];
+    var reader = new FileReader();
+
+    var imgtag = document.getElementById("displayImg");
+    imgtag.title = selectedFile.name;
+
+    reader.onload = function (event) {
+        imgtag.src = event.target.result;
+    };
+    reader.readAsDataURL(selectedFile);
+}
+const btnCancel = () => {
+    document.getElementById('displayImg').src = 'http://placehold.it/400x400';
+}
+
